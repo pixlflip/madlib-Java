d743ffe2413c9215c69d50a019962ed4f70ef8b6
blocksOfCode <jbyers2018@icloud.com>
2021-08-19 16:32:20 -0500

    init

---
+++ src/MadLib.java
@@ -0,0 +1,25 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class MadLib {
+    Scanner in = new Scanner(System.in);
+
+    /** Stories used in Generation Engine */
+
+    //syntax = [storyNum][partOfStory]
+    String storyArray[][] = {"",""};
+
+
+    String story;
+    int seed;
+
+
+    public MadLib() {
+
+    }
+
+    public get_input() {
+
+    }
+}
